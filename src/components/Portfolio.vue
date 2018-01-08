7bbf648d3378b98083fbbd3787907cbfc4406c90
Jakub Sarnowski <sarneeh@gmail.com>
2018-01-08 18:08:50 +0100

    created and extracted portfolio item to a component

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -9,39 +9,20 @@
             div(v-else)
                 div.columns
                     div.column.is-one-third(v-for="share in portfolio")
-                        div.card
-                            div.card-content
-                                    div.level
-                                        div.level-left
-                                            div.level-item
-                                                h3.title.is-4 {{share.name}}
-                                        div.level-right
-                                            div.level-item
-                                                div.tag.is-info.is-large {{ share.quantity }}
-                                            div.level-item
-                                                div.tag.is-success.is-large ${{ share.price }}
-                                                //- TODO: add switching between success/danger based on previous value
-                            div.card-footer
-                                div.card-footer-item
-                                    div.control
-                                        input.input(type="text" placeholder="Quantity")
-                                div.card-footer-item
-                                    a Sell
+                        portfolio-item(:share="share")
                             
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import PortfolioItem from './PortfolioItem.vue';
 
 export default {
     computed: {
         ...mapState(['portfolio'])
+    },
+    components: {
+        PortfolioItem
     }
 };
 </script>
-
-<style scoped>
-.stock-column {
-    padding: 20px 0px;
-}
-</style>
---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+    div.card
+        div.card-content
+                div.level
+                    div.level-left
+                        div.level-item
+                            h3.title.is-4 {{share.name}}
+                    div.level-right
+                        div.level-item
+                            div.tag.is-info.is-large {{ share.quantity }}
+                        div.level-item
+                            div.tag.is-success.is-large ${{ share.price }}
+                            //- TODO: add switching between success/danger based on previous value
+        div.card-footer
+            div.card-footer-item
+                div.control
+                    input.input(type="text", placeholder="Quantity", v-model="quantity")
+            div.card-footer-item 
+                strong ${{ sellPrice(share.price) }}
+            div.card-footer-item
+                a Sell
+                            
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    data() {
+        return {
+            quantity: 1
+        };
+    },
+    props: ['share'],
+    methods: {
+        sellPrice(price) {
+            return price * this.quantity;
+        }
+    }
+};
+</script>
