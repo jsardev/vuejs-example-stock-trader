8b70367a5047243a6088a35807833053daa8d622
Jakub Sarnowski <sarneeh@gmail.com>
2018-01-09 12:22:11 +0100

    numbers format

--- src/filters/money.js
+++ src/filters/money.js
@@ -1,11 +1,1 @@
-export default value =>
-  Array.from(value.toString())
-    .reverse()
-    .reduce((accumulator, current, index) => {
-      accumulator.unshift(current);
-      if (value >= 1000 && (index + 1) % 3 === 0) {
-        accumulator.unshift('.');
-      }
-      return accumulator;
-    }, [])
-    .join('');
+export default value => value.toLocaleString();
